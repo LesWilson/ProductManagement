1315f88a8796da00d3b707c27c3005d9f82f481d
Les Wilson <1684505+LesWilson@users.noreply.github.com>
2021-04-15 08:45:03 +0100

    Files updated as part of Section 4-2 lab

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -1,11 +1,14 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
 
 /**
  * Product class
  */
 public class Product {
+
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     /** The id of the product */
     private int id;
     private String name;
@@ -15,7 +18,7 @@ public class Product {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(final int id) {
         this.id = id;
     }
 
@@ -23,7 +26,7 @@ public class Product {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name = name;
     }
 
@@ -31,7 +34,11 @@ public class Product {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
     }
 }
